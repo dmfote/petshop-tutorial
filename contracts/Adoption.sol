d4eba8d2607985d1bd5b0ade9a430e00b9bef8f5
Dmitrii Fotesco <dmfote@microsoft.com>
2019-09-25 11:52:12 -0700

    added an event

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,15 +1,15 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.5.11;
 
 
 contract Adoption {
 
     address[16] public adopters;
-    
+    event PetAdopted(address _owner, uint _petId, uint timestamp);
     // Adopting a pet
     function adopt(uint petId) public returns (uint) {
-        
-        require(petId >= 0 && petId <= 15);
+        require(petId >= 0 && petId <= 15, 'Not withing 0-15 range');
         adopters[petId] = msg.sender;
+        emit PetAdopted(msg.sender, petId, block.timestamp);
         return petId;
     }
 
